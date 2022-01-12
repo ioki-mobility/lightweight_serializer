75110bd3e10769d0853c32ce58544d2bff4b24b2
Klaus Zanders <klaus.zanders@ioki.com>
2022-01-12 11:51:40 +0100

    Add skeleton from Gem creation

---
+++ lib/lightweight_serializer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lightweight_serializer/version"
+
+module LightweightSerializer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lightweight_serializer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LightweightSerializer
+  VERSION = "0.1.0"
+end
